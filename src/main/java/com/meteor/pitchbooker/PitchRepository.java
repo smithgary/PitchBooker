b785a76adeb5ceaf6cd020b15ef08b811aee9ebf
Gary Smith <gvsmith@deloitte.co.uk>
2020-07-21 21:30:34 +0100

    updates

---
+++ src/main/java/com/meteor/pitchbooker/PitchRepository.java
@@ -0,0 +1,10 @@
+package com.meteor.pitchbooker;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public class PitchRepository implements JpaRepository {
+
+
+}
--- src/main/java/com/meteor/pitchbooker/RootController.java
+++ src/main/java/com/meteor/pitchbooker/RootController.java
@@ -3,14 +3,24 @@ package com.meteor.pitchbooker;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RootController {
 
+    private final PitchRepository pitchRepository;
+
     @GetMapping("/pitch")
     public String indexMap(Model model){
         model.addAttribute("pitch", new Pitch());
+        return "pitch";
+    }
+    @PostMapping("/pitch")
+    public String storePitch(@ModelAttribute Pitch pitch){
+        //Store?
+        System.out.println("New pitch: " + pitch.getName());
         return "pitch";
     }
 }
